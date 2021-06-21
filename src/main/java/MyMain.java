dfc90e1fea93cad4cc2ca3d76fef738905858032
manu66449 <manu66449@gmail.com>
2021-06-21 14:08:18 +0530

    Hello

---
+++ src/main/java/MyMain.java
@@ -0,0 +1,9 @@
+public class MyMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello guys");
+        Person person = new Person("manu");
+        System.out.println(person.getName());
+    }
+
+}
